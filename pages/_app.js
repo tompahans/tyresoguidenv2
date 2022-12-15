0a7613c4e5acf5913e9ec17597868f8deb78bd74
Tommy Jämsä <hassemedh@gmail.com>
2022-12-15 11:48:29 +0100

    add bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
