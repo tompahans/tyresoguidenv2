5362faf07598a493bdaa14b9ae4cca99914f5c36
Tommy Jämsä <hassemedh@gmail.com>
2022-12-16 13:21:08 +0100

    add aktiviteter index page

--- pages/Aktiviteter/index.js
+++ pages/Aktiviteter/index.js
@@ -3,8 +3,55 @@ import { NextSeo } from "next-seo";
 export default function Aktiviteter() {
   return (
     <>
-      <NextSeo title="Aktiviteter" />
-      <div>Aktiviteter i Tyresö</div>
+      <NextSeo title="Aktiviteter i Tyresö" />
+      <div>
+        <h5>Tyresö</h5>
+        <p>
+          är en fantastisk kommun att bo i med barn i alla åldrar. Här finns en
+          mångfald av förskolor, skolor och diverse barnaktiviteter. Men hur ska
+          man veta om allt som finns att välja på? I denna artikel skapar vi en
+          lista över vissa utvalda aktiviteter som vi rekommenderar er och era
+          barn!
+        </p>
+        <h5>En plats som alla med barn under 12 år</h5>
+        <p>
+          Många barnfamiljer åker långt bort till lekländer i andra kommuner,
+          exempelvis Andy’s lekland. Inte alla vet att man på bara 5 minuter med
+          buss kan ta sig från Tyresö Centrum till Petterboda, där det
+          fantastiska leklandet Kaatach, som är minst lika bra, ligger. Där
+          finns både en inomhusdel med massor av olika roliga aktiviteter för
+          barnen och en utomhusdel, med åkattraktioner, studsmattor mm. Stora
+          som små barn älskar det!
+        </p>
+
+        <h5>Värt att nämna är även alla sportklubbar</h5>
+        <p>
+          borde veta om är leklandet Kaatach! och föreningar som finns
+          tillgänliga för barn (och ofta även vuxna) i Tyresö. Vi har Tyresö
+          Konståkningsklubb, Simsällskap, Ridskola, Handboll, Gymnastik,
+          Fotbollsförening och väldigt mycket annat. Tyresö har massvis med
+          möjligheter för barn att utvecklas inom olika sporter, vilket vi
+          tycker är väldigt viktigt.
+        </p>
+        <h5>Det finns även många bra lekplatser</h5>
+        <p>
+          och parker som kan vara roliga för barn. Ett exempel är
+          fornuddsparken, som har en stor lekplats med både traditionella
+          lekställningar och moderna innovationer.
+        </p>
+        <h5>Självklart ska man även inte glömma</h5>
+        <p>
+          möjligheterna för bad. Tyresö Aquarena har en rutschkana och
+          barnavdelningar, som de flesta barnen älskar. Det finns även möjlighet
+          för simskola i vattenhuset och givetvis finns massor med öppna
+          badplatser, som vi har beskrivit tidigare och gjort en video om.
+        </p>
+        <h5>Detta är långt ifrån allt som finns</h5>
+        <p>
+          att göra för barn i Tyresö, men det var några saker som vi tyckte var
+          värda att uppmärksamma.
+        </p>
+      </div>
     </>
   );
 }
