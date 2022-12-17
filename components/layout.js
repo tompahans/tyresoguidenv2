0757590f7f3a24ca14efe3d26c7057e56dfd2d57
Tommy Jämsä <hassemedh@gmail.com>
2022-12-17 16:45:11 +0100

    fix: add missing head tags

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import Navbar from "./navbar";
 import Header from "./header";
+import Head from "next/head";
 import { Poppins } from "@next/font/google";
 
 const poppins = Poppins({ weight: "400", subsets: "latin" });
@@ -7,6 +8,10 @@ const poppins = Poppins({ weight: "400", subsets: "latin" });
 export default function Layout({ children }) {
   return (
     <main className={poppins.className + " main"}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charset="UTF-8"></meta>
+      </Head>
       <Header />
       <Navbar />
       <div className="content">{children}</div>
