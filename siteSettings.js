e41f8c2380fd4de7b079ae57384c74c306f3ca54
Tommy Jämsä <hassemedh@gmail.com>
2022-12-17 16:43:34 +0100

    feat: add siteSettings

---
+++ siteSettings.js
@@ -0,0 +1,66 @@
+export const menuItems = [
+  {
+    label: "Hem",
+    url: "/",
+    title: "Hem",
+  },
+  {
+    label: "Badplatser",
+    url: "/Badplatser",
+    title: "Badplatser",
+  },
+  {
+    label: "Restauranger",
+    url: "/Restauranger",
+    title: "Restauranger",
+  },
+  {
+    label: "Historia",
+    url: "/Historia",
+    title: "Historia",
+    sublabels: [
+      {
+        label: "Tyresö",
+        url: "/Historia",
+        title: "Tyresö historia",
+      },
+      {
+        label: "Annas Pepparkakor",
+        url: "/Historia/Annas",
+        title: "Annas Pepparkakor historia",
+      },
+      {
+        label: "Fontana Food",
+        url: "/Historia/Fontana",
+        title: "Fontana Food historia",
+      },
+      {
+        label: "Tyresö Slott",
+        url: "/Historia/Slottet",
+        title: "Tyresö Slott historia",
+      },
+    ],
+  },
+  {
+    label: "Aktiviteter",
+    url: "/Aktiviteter",
+    title: "/Aktiviteter",
+    sublabels: [
+      {
+        label: "Alby Fridluftsgård",
+        url: "/Aktiviteter/Alby",
+        title: "Besök Alby Fridluftsgård",
+      },
+      {
+        label: "Tyresö Slott",
+        url: "/Aktiviteter/Slottet",
+        title: "Besök Tyresö Slott",
+      },
+      {
+        label: "För Barnfamiljen",
+        url: "/Aktiviteter/Barnfamiljen",
+        title: "Aktiviteter för barnfamiljen",
+      },
+    ],
+  },
+];
